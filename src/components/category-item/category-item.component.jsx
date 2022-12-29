4f52c0673c5127e9a24c5d488cb84600a8b99918
Erin Shields <elshields92@gmail.com>
2022-12-29 16:16:57 -0300

    add React Router and Navigation component

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -1,7 +1,7 @@
 import "./category-item.styles.scss";
 
 const CategoryItem = ({ category }) => {
-  const { id, imageUrl, title } = category;
+  const { imageUrl, title } = category;
   return (
     <div className="category-container">
       <div
---
+++ src/routes/shop/shop.component.jsx
@@ -0,0 +1,5 @@
+const Shop = () => {
+  return <h1>I am the shop component.</h1>;
+};
+
+export default Shop;
